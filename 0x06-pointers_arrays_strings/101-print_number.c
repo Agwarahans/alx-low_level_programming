290dacad10bceaae68284deb043be2f03c5a3abc
Agwarahans <jhann395@gmail.com>
2023-07-02 03:12:55 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,22 +3,24 @@
 /**
  * print_number - print numbers chars
  * @n: integer params
- * Return: void
+ * Return: 0
  */
+
 void print_number(int n)
 {
 	unsigned int n1;
 
-	n1 = (n < 0) ? -n : n;
+	n1 = n;
 
 	if (n < 0)
+	{
 		_putchar('-');
+		n1 = -n;
+	}
 
 	if (n1 / 10 != 0)
+	{
 		print_number(n1 / 10);
-
+	}
 	_putchar((n1 % 10) + '0');
-
-	if (n == 0)
-		_putchar('0');
 }
