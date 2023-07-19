a423fb11d88e66cc571e639740a7f4457f8e5188
Agwarahans <jhann395@gmail.com>
2023-07-19 07:35:48 -0700

    6-is_prime_number.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,7 @@
+#include "main.h"
+/**
+  *_strlen_recursion - returns length of a string
+  */
+int _strlen_recursion(char *s)
+{
+	if (*s =='\0')
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,30 +1,22 @@
 #include "main.h"
-
-int check__prime(int n, int i);
-
 /**
- * is_prime_number - Return if a number is prime
- * @n: number to check
- * Return: 1 if n is a prime number, 0 if not
- */
-int is_prime_number(int n)
+  *prime - calculate if its prime
+  *@n:number to check
+  *@i: iterator
+  *Return: 0
+  */
+int prime(int n, int i)
 {
-	if (n <= 1)
+	if (n <= 1 || (n != i && n % i == 0))
+	{
 		return (0);
-	return (check__prime(n, n - 1));
-}
-
-/**
- * check_prime -check for prime number
- * @n: number to check
- * @i: iterator
- * Return: 1 if n is prime, 0 if not
- */
-int check_prime(int n, int i)
-{
-	if (i == 1)
+	}
+	else if (n == i)
+	{
 		return (1);
-	if (n % i == 0 && i > 0)
-		return (0);
-	return (actual_prime(n, i - 1));
+	}
+	return (prime(n, i + 1));
 }
+/**
+  *check__prime - check for prime number
+  *@n: number to check
