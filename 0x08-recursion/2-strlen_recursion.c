db32ee206d7e504038c6d7df24c572e4d99e132a
Agwarahans <jhann395@gmail.com>
2023-09-05 23:49:43 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,7 +3,7 @@
  * _strlen_recursion - calculates the length of a string
  * @s: string to be calculated
  *
- *Return: calculated length of string 
+ *Return:calculated length of string 
  */
 int _strlen_recursion(char *s)
 {
