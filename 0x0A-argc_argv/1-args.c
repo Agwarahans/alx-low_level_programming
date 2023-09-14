30c9ceab9778be658b077e25df1552391e69d7e3
Agwarahans <jhann395@gmail.com>
2023-09-14 03:38:00 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,10 +1,11 @@
+
 #include <stdio.h>
 #include "main.h"
 
 /**
- * main - print the number of CL arguments 
- * @argc: number of CL arguments
- * @argv: array that has cl arguments
+ * main - print the num of command line arguments passed to the program
+ * @argc: number of command line arguments
+ * @argv: array of comand line  arguments
  *
  * Return: Always 0 (Success)
  */
